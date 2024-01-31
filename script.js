5543b4db487654167e214ed41e1d814668b4d98f
Alexz Desktop <chikofarrel@gmail.com>
2024-02-01 00:47:46 +0700

    fixing bug and just change the content

--- script.js
+++ script.js
@@ -2,9 +2,15 @@ const buttons = document.querySelectorAll(".btn");
 const title = document.querySelector(".title");
 const image = document.querySelector(".image");
 
-const question = ["Halo1", "Halo2", "Halo3", "Halo4", "Halo5", "Halo6"];
+const question = [
+  "Haii Aku Mau Tanya Sesuatu...",
+  "Kamu uda siap dengerin nya??",
+  "Setelah baca ini aku harap<br>jangan marah ya..?",
+  "Dijawab dengan jujur okei..?",
+  "Daniella Lolita Charlie..<br> will you be my girlfriend?",
+  "Ekspresiku saat ini...",
+];
 
-let countNo = 0;
 let countYes = 0;
 let countTitle = 0;
 let xTes, yTes;
@@ -32,7 +38,36 @@ function getRandomY() {
   return coba;
 }
 
+document.addEventListener("DOMContentLoaded", () => {
+  title.style.animation = "typing 3.5s steps(40, end)";
+  if (countTitle === 0) {
+    title.innerHTML = question[countTitle];
+    buttons.forEach((button) => {
+      if (button.id === "btn-no") {
+        button.style.display = "none";
+      } else {
+        button.innerText = "Apaa?";
+      }
+    });
+    countTitle = 1;
+  }
+  setTimeout(() => {
+    title.style.animation = "none";
+    buttons.forEach((button) => {
+      button.classList.add("fadeIn");
+      if (button.id == "btn-yes") {
+      }
+    });
+  }, 3500);
+});
+
 function updateButtonDisplay() {
+  if (countTitle != 5) {
+    title.style.animation = "typing 3.5s steps(40, end)";
+    setTimeout(() => {
+      title.style.animation = "none";
+    }, 3500);
+  }
   buttons.forEach((button) => {
     if (button.id === "btn-no") {
       button.style.display = countTitle === 5 ? "block" : "none";
@@ -43,31 +78,22 @@ function updateButtonDisplay() {
   });
 }
 
-if (countTitle === 0) {
-  title.innerHTML = question[countTitle];
-  buttons.forEach((button) => {
-    if (button.id === "btn-no") {
-      button.style.display = "none";
-    } else {
-      button.innerText = "Apaa?";
-    }
-  });
-  countTitle++;
-}
-
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
-    // let x = Math.floor(Math.random() * 50) + 50;
-    // let y = Math.floor(Math.random() * 100) + 100;
+    console.log(countTitle);
+
+    if (countTitle >= 4) {
+      button.style.opacity = 1;
+    } else {
+      button.style.transition = "none";
+      button.classList.remove("fadeIn");
+    }
+
     let randomX = Math.floor(Math.random() * 100) + 1;
     let randomY = Math.floor(Math.random() * 100) + 1;
 
-    if (button.id === "btn-no") {
-      countNo++;
-      button.style.transform = `translate(${
-        randomX > 50 ? "-" : ""
-      }${getRandomX()}px, ${randomY > 50 ? "" : "-"}${getRandomY()}px)`;
-    } else {
+    if (button.id === "btn-yes") {
+      console.log("yes di klik");
       countYes++;
       title.innerHTML = question[countTitle];
       countTitle++;
@@ -77,10 +103,19 @@ buttons.forEach((button) => {
       if (countTitle === 5) {
         button.style.transform = `translate(-50px, 0px)`;
       }
+      if (countTitle != 5) {
+        setTimeout(() => {
+          button.style.transition = "all 0.5s";
+          button.classList.add("fadeIn");
+        }, 3500);
+      }
+    } else if (button.id == "btn-no") {
+      console.log("no di klik");
+      button.style.transform = `translate(${
+        randomX > 50 ? "-" : ""
+      }${getRandomX()}px, ${randomY > 50 ? "" : "-"}${getRandomY()}px)`;
     }
 
     updateButtonDisplay();
   });
 });
-
-// aku mau tanya sesuatu, kamu uda siap dengerinnya?, setelah baca jangan marah ya, dijawab jujur okei..?
